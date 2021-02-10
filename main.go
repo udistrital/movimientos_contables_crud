f3b13a9abfb1d7d214131cb35e794d8e9b8d1102
criss201x <cryteck_1011@hotmail.com>
2021-02-10 09:42:13 -0500

    feat: junto con @JocoolQ se genera api y se agrega archivo ddl #2

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	_ "github.com/udistrital/movimientos_contables_crud/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	orm.Debug = true
+	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	beego.Run()
+}
---
+++ routers/router.go
@@ -0,0 +1,32 @@
+// @APIVersion 1.0.0
+// @Title beego Test API
+// @Description beego has a very cool tools to autogenerate documents for your API
+// @Contact astaxie@gmail.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+package routers
+
+import (
+	"github.com/udistrital/movimientos_contables_crud/controllers"
+
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	ns := beego.NewNamespace("/v1",
+
+		beego.NSNamespace("/transaccion",
+			beego.NSInclude(
+				&controllers.TransaccionController{},
+			),
+		),
+
+		beego.NSNamespace("/movimiento",
+			beego.NSInclude(
+				&controllers.MovimientoController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
+}
